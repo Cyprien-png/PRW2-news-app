7a51950f0f4932565139bd6ddec9983d8ee3b5b5
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-01-19 15:11:54 +0100

    Fix method called on the form to bid

--- resources/views/auctions/create.blade.php
+++ resources/views/auctions/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Nouvelle enchère</h1>
-    <form method="POST" action="{{ route('articles.comments.store', $article) }}">
+    <form method="POST" action="{{ route('articles.auctions.store', $article) }}">
         @csrf
         @include('auctions.fields')
         <input type="submit" value="Ajouter">
