ac0445246a9a729b1e71009b3b17ba289abedfa9
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-01-19 14:25:54 +0100

    Add view count on the index view

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,7 +4,7 @@
     <h1>Voici les articles</h1>
     <ul>
     @foreach ($articles as $article)
-        <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a></li>
+        <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a> vu {{ $article->view_count }} fois</li>
     @endforeach
     </ul>
 @endsection
