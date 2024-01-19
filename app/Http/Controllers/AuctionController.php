375688144f54450a5117ae32fa22eb60edc0aeda
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-01-19 15:12:56 +0100

    Add auctions storing method

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Auction;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class AuctionController extends Controller
 {
-    public function create()
+    public function create(Article $article)
     {
-        return view('auctions.create', ['auction' => new Auction()]);
+        return view('auctions.create', ['auction' => new Auction(), 'article' => $article]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Article $article, Request $request)
     {
-        //
+        if ($article->archived_at)
+            abort(403);
+
+        $article->auctions()->create($request->all());
+        return redirect()->route('articles.show', $article);
     }
 }
